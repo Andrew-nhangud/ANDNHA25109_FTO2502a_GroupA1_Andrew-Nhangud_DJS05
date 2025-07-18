85bdd5525afe346b90f760b160558f798d2f8e1d
Andrew Nhnagud <Andrewnhangud3785@gmail.com>
2025-07-18 15:27:04 +0200

    Wrap your main.jsx component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/styles.css';
 import { PodcastProvider } from './PodcastContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PodcastProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </PodcastProvider>
   </React.StrictMode>
 );
